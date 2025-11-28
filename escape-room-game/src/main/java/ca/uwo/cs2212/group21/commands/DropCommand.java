2052e3a0f9dae11383cb9d971f2b2cee196467a8
Nadia Dollesin Parhizgar <nparhizg@uwo.ca>
2025-11-27 20:57:50 -0500

    implemented PickUpCommand and DropCommand - added GameEngine Methods - added temporary TalkCommand fix so project can compile/test

--- escape-room-game/src/main/java/ca/uwo/cs2212/group21/commands/DropCommand.java
+++ escape-room-game/src/main/java/ca/uwo/cs2212/group21/commands/DropCommand.java
@@ -1,5 +1,29 @@
 package ca.uwo.cs2212.group21.commands;
 
+import ca.uwo.cs2212.group21.model.GameState;
+import ca.uwo.cs2212.group21.model.Item;
+import ca.uwo.cs2212.group21.model.Room;
+
+// this command handles dropping an item from player's inventory back into the room
 public class DropCommand {
+    public String execute(GameState state, String itemName){
+
+        //only drop items that are currently in inventory already. this covers following cases:
+        //1. dropping item immediately after picking up
+        //2. dropping item from inventory screen later
+        Item item = state.findItemInventory(itemName);
+
+        if (item==null){
+            return itemName + " is not in your inventory.";
+        }
+
+        // remove item from inventory
+        state.removeItemFromInventory(item);
+
+        //put/drop item back into room
+        Room room = state.getCurrentRoom();
+        room.addItem(item);
+        return item.getName() + " dropped.";
+    }
     
 }
